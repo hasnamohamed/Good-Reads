b5503127c5e53995fb3bf5bd7940dedf0003a2bc
MohamedAl-Attar <mohamedelattar28@yahoo.com>
2023-03-11 06:10:18 +0200

    extra files

--- nodejs/controllers/categoriesController.js
+++ nodejs/controllers/categoriesController.js
@@ -1,110 +1,72 @@
-const Book = require('../models/book.js')
+const Category = require('../models/category.js')
 
-const getBooks = (async (req, res) => {
-    Book.find((err, data) => {
-        if (!err) {
-            res.status(200).send(data)
-        } else {
-            res.status(400).send('something went wrong : ' + err)
-        }
-    });
-})
-
-const getBook = (async (req, res) => {
+const getCategories = (async function (req, res) {
     try {
-        const bookID = req.params.id
-        const book = await Book.findOne({_id:bookID});
-        if(!book){
-            return res.status(400).json({message:'Book not found'})
-        }
-        res.status(200).json(book)
-    } catch (err) {
-        res.status(400).send('something went wrong : ' + err)
+        const Cats = await Category.find({})
+        res.json(Cats)
+    } catch (error) {
+        res.status(400).json(error.message);
     }
 })
 
-const createBook = (async function (req, res) {
+const getCategory = (async function (req, res) {
     try {
-        if (!req.body.title || !req.body.image || !req.body.description || !req.body.authorId || !req.body.catId) {
-            return res.status(400).json({ message: 'Missing required fields' });
-          }
-        data = new Book();
-        data.title = req.body.title,
-        data.image = req.body.image,
-        data.description = req.body.description,
-        data.authorId = req.body.authorId,
-        data.catId = req.body.catId,
-        data.reviews = req.body.reviews,
-        await data.save()
-        res.status(200).send("Book saved successfully")
+        const catId = req.params.id;
+        const cat = await Category.findOne({ _id: catId })
+        if (!cat) {
+            res.status(400).json({ message: 'Category not found' })
+        }
+        else {
+            res.status(200).send(cat);
+        }
+    } catch (err) {
+        res.status(400);
+        res.send(err);
+    }
+})
+
+const createCategory = (async function (req, res) {
+    try {
+        let alldata = new Category();
+        alldata.name = req.body.name;
+        await alldata.save()
+        res.send("Category saved successfully")
     } catch (err) {
         if (err.name === "ParallelSaveError") {
-            res.status(400).send("Parallel Save Error" + err);
+            console.log("There was a parallel save error for", keyA, keyB);
+        }
+    }
+
+})
+
+const updateCategory = (async (req, res) => {
+  try{
+    const cat = await Category.findOne({_id:req.params.id});
+    cat.name = req.body.name;
+    await cat.save();
+    res.status(200).send("Category Name Updated Successfully")
+  }catch(err){
+    res.status(400).send(err.message);
+  }
+})
+const deleteCategory = (async (req, res) => {
+    try{
+        const count = await Category.deleteOne({_id:req.params.id});
+        if(count === 0){
+            res.status(400).send("Category Not Found")
         }
         else{
-            res.status(400).send("Something went wrong" + err);
+            res.status(200).send("Category Deleted Successfully")
         }
+
+    }catch(err){
+        res.status(400).send(err.message);
     }
-})
-
-const updateBook = (async (req, res) => {
-    try {
-        const bookId = req.params.id;
-        if (!req.body.title || !req.body.image || !req.body.description || !req.body.authorId || !req.body.catId) {
-            return res.status(400).json({ message: 'Missing required fields' });
-          }
-        const {
-            title,
-            image,
-            description,
-            authorId,
-            catId
-        } = req.body;
-        
-        const updatedBook = await Book.findByIdAndUpdate(
-            bookId, {
-                title,
-                image,
-                description,
-                authorId,
-                catId
-            }, {
-                new: true
-            }
-        );
-        if (!updatedBook) {
-            return res.status(404).json({
-                message: 'Book not found'
-            });
-        }
-        return res.status(200).json("Book updated");
-    } catch (err) {
-        res.status(400).send(err);
-    }
-
-})
-const deleteBook = (async (req, res) => {
-    try {
-        const bookId = req.params.id
-        const result = await Book.deleteOne({
-            _id: bookId
-        })
-
-        if (result.Count === 0) {
-            return res.status(400).json({
-                message: 'Book not found'
-            })
-        }
-        res.status(200).json(result)
-    } catch (err) {
-        res.status(200).send(err);
-    }
-
 })
 module.exports = {
-    getBooks,
-    getBook,
-    createBook,
-    updateBook,
-    deleteBook
+    getCategories,
+    getCategory,
+    createCategory,
+    updateCategory,
+    deleteCategory
 }

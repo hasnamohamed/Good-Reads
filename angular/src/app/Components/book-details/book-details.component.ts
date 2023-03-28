1cfd9bea5400674fe62cd282b962568237302f74
hasnamohamed <hasnaamohammed538@gmail.com>
2023-03-28 06:19:47 +0200

    adding new features in books-details component

--- angular/src/app/Components/book-details/book-details.component.ts
+++ angular/src/app/Components/book-details/book-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { IBook } from 'src/Models/ibook';
 import { BookService } from 'src/Services/books.service';
@@ -11,20 +11,28 @@ import {StarRatingColor} from "../Shared/star-rating/star-rating.component";
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent {
-  book: any| null = null
+  @Input('book') book: any = {};
+  author:IAuthor={
+    "name":"auth1",
+    "birth":new Date,
+    "bio":"new bio "
+  };
+  authorId:any;
   book_id:string
    starCount:number=5;
+  userRating:number=3;
   starColor: StarRatingColor = StarRatingColor.accent;
   starColorP: StarRatingColor = StarRatingColor.primary;
   starColorW: StarRatingColor = StarRatingColor.warn;
 
   onRatingChanged(rating: any) {
-    this.book.rating.rate = rating.rating;
+    this.userRating= rating.rating;
   }
   constructor(
     private active_route:ActivatedRoute,
     private book_service:BookService,
-    private router:Router
+    private router:Router,
+    private author_service:AuthorServiceService
   ){
     this.book_id = this.active_route.snapshot.paramMap.get('id') ?? ''
   }
@@ -32,6 +40,11 @@ export class BookDetailsComponent {
     this.book_service.getBookById(this.book_id).subscribe(book=>{
       this.book = book
     })
+  }
+  @ViewChild('status') status!: ElementRef;
+  selectedStatus = 0;
+  onSelected(): void {
+    this.selectedStatus = this.status.nativeElement.value;
   }
   deleteBook(){
     this.book_service.deleteBook(this.book_id).subscribe(book=>{
--- angular/src/app/Components/books/book-card/book-card.component.ts
+++ angular/src/app/Components/books/book-card/book-card.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {StarRatingColor} from "../../Shared/star-rating/star-rating.component";
+import {BookService} from "../../../../Services/books.service";
+import {AuthorServiceService} from "../../../../Services/author-service.service";
+import {IAuthor} from "../../../../Models/iauthor";
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
@@ -11,10 +14,23 @@ export class BookCardComponent implements OnInit {
   starColor: StarRatingColor = StarRatingColor.accent;
   starColorP: StarRatingColor = StarRatingColor.primary;
   starColorW: StarRatingColor = StarRatingColor.warn;
-
-  ngOnInit() {
+  author:IAuthor={
+    "name":"auth1",
+    "birth":new Date,
+    "bio":"new bio "
+  };
+  authorId:any;
+  constructor(private author_service:AuthorServiceService) {
   }
-
+  ngOnInit() {
+    this.fetchAuthorsData()
+    console.log(this.authorId)
+  }
+  fetchAuthorsData(){
+    this.author_service.getAuthorById(this.authorId).subscribe(response=>{
+      console.log(response)
+    })
+  }
   @ViewChild('status') status!: ElementRef;
   selectedStatus = 0;
 

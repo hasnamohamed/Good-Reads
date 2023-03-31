1145f07112ad761085bd0418b96c359852d57d1f
mohmedsabryy <m7mdsabryyy@gmail.com>
2023-03-31 14:09:06 +0200

    get admin changes

--- angular/src/Models/ibook.ts
+++ angular/src/Models/ibook.ts
@@ -1,13 +1,13 @@
 export interface IBook {
-    _id?:string;
-    rating?:{
-      rate:number,
-      totalVotes: number,
-      totalPoints: number,
-    }
-    title:string;
-    description:string;
-    image:string;
-    authorId:string
-    catId:string
+  _id?:string;
+  rating?:{
+    rate:number,
+    totalVotes: number,
+    totalPoints: number,
   }
+  title:string;
+  description:string;
+  image:string;
+  authorId:string
+  catId:string
+}
